3e0b68c0a5244ae819aa5c829e044626ff1f6a08
MariaShev777 <masha_shevtsova@tut.by>
2024-01-20 19:38:42 +0300

    feat: sign-in UPD & sign-in.stories.tsx & Storybook Addon React Router v6

--- src/components/auth/sign-in/sign-in.tsx
+++ src/components/auth/sign-in/sign-in.tsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom'
 
-import { Button, Card, ControlledCheckbox, ControlledTextField, Typography } from '@/components'
-import { SignInFormValues, useSignIn } from '@/components/auth/sign-in/useSignIn'
-import { DevTool } from '@hookform/devtools'
+import {
+  Button,
+  Card,
+  ControlledCheckbox,
+  ControlledTextField,
+  SignInFormValues,
+  Typography,
+  useSignIn,
+} from '@/components'
 
 import s from './sign-in.module.scss'
 
@@ -47,13 +53,12 @@ export const SignIn = ({ onSubmit }: Props) => {
         </Button>
       </form>
 
-      <DevTool control={control} />
       <div className={s.signUp}>
         <Typography variant={'body2'}>Don&apos;t have an account?</Typography>
 
-        <Button as={Link} to={'/sign-up'} variant={'link'}>
+        <Typography as={Link} className={s.signUpLink} to={'/sign-up'} variant={'link1'}>
           Sign up
-        </Button>
+        </Typography>
       </div>
     </Card>
   )
