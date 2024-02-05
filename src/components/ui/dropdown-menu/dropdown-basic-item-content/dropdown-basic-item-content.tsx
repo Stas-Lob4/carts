1ed18e0453062115f4f12f6d85e78e147d81adc9
Stas <staslob4uck@gmail.com>
2024-02-05 22:57:15 +0100

    refactoring component/user-dropdown

--- src/components/ui/dropdown-menu/dropdown-basic-item-content/dropdown-basic-item-content.tsx
+++ src/components/ui/dropdown-menu/dropdown-basic-item-content/dropdown-basic-item-content.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react'
 
-import s from './dropdown-basic-item-content.module.scss'
 import { Typography } from '@/components'
+
+import s from './dropdown-basic-item-content.module.scss'
 
 type DropdownBasicItemContentProps = {
   icon: ReactNode
@@ -10,10 +11,11 @@ type DropdownBasicItemContentProps = {
 
 export const DropdownBasicItemContent = (props: DropdownBasicItemContentProps) => {
   const { icon, name } = props
+
   return (
     <>
       <div className={s.icon}>{icon}</div>
-      <Typography variant={'caption'}>{name}</Typography>
+      <Typography variant={'body1'}>{name}</Typography>
     </>
   )
 }
